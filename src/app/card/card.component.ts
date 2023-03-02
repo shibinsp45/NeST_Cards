d382e2326ec2147ac91191d503b29131f5025df5
SHIBIN <shibinsp45@gmail.com>
2023-03-03 00:55:39 +0530

    first commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { district } from 'src/assets/data';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  districts = district
+}
---
+++ src/assets/data.ts
@@ -0,0 +1,59 @@
+export const district = [
+    {
+        name: 'Alappuzha',
+        img: 'alappuzha.jpg'
+    },
+    {
+        name: 'Ernakulam',
+        img: 'ernakulam.jpg'
+    },
+    {
+        name: 'Idukki',
+        img: 'idukki.jpg'
+    },
+    {
+        name: 'Kannur',
+        img: 'kannur.jpeg'
+    },
+    {
+        name: 'Kasargod',
+        img: 'kasargod.jpeg'
+    },
+    {
+        name: 'Kollam',
+        img: 'kollam.jpeg'
+    },
+    {
+        name: 'Kottayam',
+        img: 'kottayam.jpeg'
+    },
+    {
+        name: 'Kozhikode',
+        img: 'kozhikode.jpeg'
+    },
+    {
+        name: 'Malappuram',
+        img: 'malappuram.jpeg'
+    },
+    {
+        name: 'Palakkad',
+        img: 'palakkad.jpeg'
+    },
+    {
+        name: 'Pathanamthitta',
+        img: 'pathanamthitta.jpeg'
+    },
+    {
+        name: 'Thrissur',
+        img: 'thrissur.jpeg'
+    },
+    {
+        name: 'Thiruvananthapuram',
+        img: 'trivandrum.jpeg'
+    },
+    {
+        name: 'Wayanad',
+        img: 'wayanad.jpeg'
+    }
+
+]
